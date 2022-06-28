8808b55b5dd1c98508c6bfeacb81d7f8ea07f3a6
Tatsiana Dziatkowic <tatiana.detkovich@yandex.ru>
2022-06-28 21:01:37 +0300

    feat: add solution of the reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  if (n < 0) {
+    return Number(n.toString().split('').reverse().join('').replace('-', ''));
+  }
+
+  else {
+    return Number(n.toString().split('').reverse().join(''));
+  }
 }
